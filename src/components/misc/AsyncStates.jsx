c8c5c87fbc5d59c77a7db1a594e515a878d300f9
pnoul <pavlos.noulis@gmail.com>
2023-06-07 16:53:39 +0300

    removed unnessecary useeffect hook

--- src/components/misc/AsyncStates.jsx
+++ src/components/misc/AsyncStates.jsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 
 function AsyncStates({ state, onPending, onResolved, onRejected, children }) {
+  React.useEffect(() => {
+    console.log("ASYNC STATES STATE CHANGE");
+    console.log(state);
+  }, [state]);
   switch (state) {
     case "pending":
       return <>{onPending || <p>pending</p>}</>;
